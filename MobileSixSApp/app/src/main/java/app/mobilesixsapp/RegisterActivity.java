27b4ab6ebe8106aea0b4d0a80202c28a718b0619
westj20 <112105864+westj20@users.noreply.github.com>
2023-10-25 13:53:42 -0500

    update10252023

---
+++ MobileSixSApp/app/src/main/java/app/mobilesixsapp/RegisterActivity.java
@@ -0,0 +1,27 @@
+package app.mobilesixsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    EditText edUsername, edEmail, edPassword, edConfirm;
+    Button Reg, Back;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        edUsername = findViewById(R.id.editTextRegUsername);
+        edPassword = findViewById(R.id.editTextRegPassword);
+        edEmail = findViewById(R.id.editTextRegEmail);
+        edConfirm = findViewById(R.id.editTextRegConfirmPassword);
+        Reg = findViewById(R.id.buttonRegister);
+        Back = findViewById(R.id.buttonBack);
+    }
+}
